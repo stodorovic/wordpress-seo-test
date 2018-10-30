b40d70781adac5e0d76c2c3928afc9f5a5c1ad6b
stodorovic <sasa.todorovic@gmx.com>
2018-10-30 06:20:56 +0100

    test 22

--- tests/doubles/class-wpseo-woocommerce-shop-page-double.php
+++ tests/doubles/class-wpseo-woocommerce-shop-page-double.php
@@ -5,7 +5,7 @@
  * @package WPSEO\Frontend
  */
 
-class WPSEO_WooCommerce_Shop_Page_Double extend WPSEO_WooCommerce_Shop_Page {
+class WPSEO_WooCommerce_Shop_Page_Double extends WPSEO_WooCommerce_Shop_Page {
 
 	private function is_woo_activated() {
 		return true;
