11b4d4eb60744f363b2849100d69517b27fd5066
Sasa Todorovic <sasa.todorovic@gmx.com>
2023-07-08 16:40:20 +0200

    Use sanitize_url to sanitize canonical field

--- admin/taxonomy/class-taxonomy.php
+++ admin/taxonomy/class-taxonomy.php
@@ -208,9 +208,10 @@ class WPSEO_Taxonomy {
 		foreach ( WPSEO_Taxonomy_Meta::$defaults_per_term as $key => $default ) {
 			// phpcs:ignore WordPress.Security.NonceVerification.Missing -- Reason: Nonce is already checked by WordPress before executing this action.
 			if ( isset( $_POST[ $key ] ) && is_string( $_POST[ $key ] ) ) {
+				// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized -- We're preparing to do just that.
 				$data = wp_unslash( $_POST[ $key ] );
 
-				$new_meta_data[ $key ] = WPSEO_Utils::sanitize_url( $data );
+				$new_meta_data[ $key ] = ( $key !== 'wpseo_canonical' ) ? WPSEO_Utils::sanitize_text_field( $data ) : WPSEO_Utils::sanitize_url( $data );
 			}
 
 			// If analysis is disabled remove that analysis score value from the DB.
